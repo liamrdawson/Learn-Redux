35fb0704d144a85281a520d957e0035b9cd30054
Liam Dawson <liamrdawson@gmail.com>
2019-12-01 15:14:18 +0000

    Demonstrate and ACTION, REDUCER & DISPATCH in action. Using the .dispatch method I am incrementing and decrementing the counter state, which is held in store via createStore(counter) and logging each dispatch to the console

--- dev-ed/learn-redux/src/index.js
+++ dev-ed/learn-redux/src/index.js
@@ -3,6 +3,42 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import {createStore} from 'redux';
+
+//  STORE -> GLOBALIZED STATE
+
+//  ACTION INCREMENT
+const increment = () => {
+    return {
+        type: 'INCREMENT'
+    }
+}
+
+const decrement = () => {
+    return {
+        type: 'DECREMENT'
+    }
+}
+
+//  REDUCER
+const counter = (state = 0, action) => {
+    switch(action.type) {
+        case 'INCREMENT':
+            return state + 1;
+        case 'DECREMENT':
+            return state - 1;
+    }
+}
+
+let store = createStore(counter);
+
+//  Display this in console...
+store.subscribe(() => console.log(store.getState()));
+
+//  DISPATCH
+store.dispatch(increment());
+store.dispatch(decrement());
+store.dispatch(decrement());
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
